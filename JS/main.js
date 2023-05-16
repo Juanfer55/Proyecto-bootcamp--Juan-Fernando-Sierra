bd8154a553565478f768f0169b43abb41312f112
Juanfer55 <juanfersierra07@gmail.com>
2023-05-15 23:32:29 -0500

    15/05/2023

--- JS/main.js
+++ JS/main.js
@@ -4,11 +4,4 @@ const barraDeNav = document.getElementById('barra-de-nav');
 botonMenuHeader.addEventListener('click', function() {
   barraDeNav.classList.toggle('menu-plegado');
 });
-
-const elementosMenu = document.getElementsByClassName('barra-de-navegacion');
-for (let i = 0; i < elementosMenu.length; i++) {
-  elementosMenu[i].addEventListener('click', function() {
-    barraDeNav.classList.add('menu-plegado');
-  });
-}
-// Para hacer este código invesigué porque no tengo mucho conocimiento en JavaScript//
+// Para hacer este código para el menú plegable investigué en internet porque no tengo mucho conocimiento en JavaScript//
