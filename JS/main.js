aa0b35296d6dac9a9004ebf8bc37ce84954bba16
Juanfer55 <juanfersierra07@gmail.com>
2023-05-16 18:38:51 -0500

    16/05/2023

--- JS/main.js
+++
@@ -1,6 +0,0 @@
-const botonMenuHeader = document.getElementById('boton-menu-header');
-const barraDeNav = document.getElementById('barra-de-nav');
-
-botonMenuHeader.addEventListener('click', function() {
-  barraDeNav.classList.toggle('menu-plegado');
-});
